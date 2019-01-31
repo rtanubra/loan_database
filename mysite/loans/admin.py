34e607098c0f7208c3f066273ceb04de757d8a83
rtanubra <rdtanubrata@gmail.com>
2019-01-31 17:31:01 -0500

    First commit on models for payments

--- mysite/loans/admin.py
+++ mysite/loans/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Loan
+from payments.models import Payment
 
 admin.site.register(Loan)
+admin.site.register(Payment)
--- mysite/payments/models.py
+++ mysite/payments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from datetime import date
+from loans.models import Loan
 # Create your models here.
+
+class Payment(models.Model):
+    def __str__(self):
+        return f"Payment {self.payment_loan.loan_name} - {self.payment_date}"
+    payment_loan = models.ForeignKey(Loan,on_delete=models.CASCADE)
+    payment_amount = models.DecimalField(max_digits=7, decimal_places=2)
+    payment_date = models.DateField(default=date.today())
